c944d4eec050e7126105864a7f0194e6f4150674
Giuseppe <giuse88@gmail.com>
2014-05-24 10:07:45 +0100

    Sort of login with google.

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,40 +1,35 @@
 Template.login.events({
 
-  'click #submit-btn' : function(event, $target){
-    // prevent submit form
-    //event.preventDefault();
-    // retrieve the input field values
-    console.log('test')
-    console.log(' Login ' + $('#login-form').valid());
-    /*
-    // Trim and validate your fields here....
-    // If validation passes, supply the appropriate fields to the
-    Meteor.loginWithPassword(email, password, function(err){
-      if (err) {
-        // The user might not have been found, or their passwword
-        // could be incorrect. Inform the user that their
-        // login attempt has failed.
-        console.log("Error when logging ");
-        console.log(err.reason);
-        // TODO
-        // error rendering
-      } else {
-        // The user has been logged in.
-        console.log("user logged in");
-        /*
-         This is a problem
-         Router.go('/library');
-      }
+  'click #submit-btn' : function(event, $target) {
+    if ($('#login-form').valid()) {
+      //
+      var email = $('#email').value;
+      var password = $('#email').password;
+      //
+      Meteor.loginWithPassword(email, password, function (err) {
+        //
+        if (err) {
+          console.log("Error during login: ", err.reason);
+          // TODO error rendering
+        } else {
+          console.log("Login sucessful")
+        }
+        //
+      });
+      //
+    }
+  },
+
+  'click #google-btn' : function () {
+    Meteor.loginWithGoogle(['email', 'profile'], function(obj) {
+      console.log(obj);
     });
-    */
-    return false;
   }
 
 });
 
 Template.login.rendered = function () {
 
-  console.log('test');
   $('#login-form').validate({
     debug: true,
 
@@ -49,7 +44,6 @@ Template.login.rendered = function () {
     },
 
     invalidHandler: function (event, validator) {
-      //display error alert on form submit
     },
 
     errorPlacement: function (label, element) { // render error placement for each input type
---
+++ server/login-services/google_services.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "1010747444263-5nojdnqf5ove6uifv0linvmfl8gphj6o.apps.googleusercontent.com",
+  secret: "WJ4lWRW3ozW667sv8n5JjnIO"
+});
