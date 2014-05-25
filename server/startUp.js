69fc85c5a0c45f3598f8a10ea6270bc9387fa882
Giuseppe <giuse88@gmail.com>
2014-05-25 23:34:21 +0100

    email verification.

--- server/startUp.js
+++ server/startUp.js
@@ -1,6 +1,10 @@
 Meteor.startup(function () {
   process.env.MAIL_URL = 'smtp://postmaster%40squiddy.io:2lnd4eu59ot4@smtp.mailgun.org:587';
 
+  Accounts.urls.verifyEmail = function(token) {
+    return Meteor.absoluteUrl('verify-email/' + token);
+  };
+
   Meteor.methods({
 
     'squiddyCreateUser' : function(registrationForm) {
